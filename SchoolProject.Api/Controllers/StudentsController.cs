86d1a973b3f24f08db35b725e6995c49d380172e
Murad Alkhalifi <msn0033@gmail.com>
2023-10-12 13:15:29 +0300

    11.Handle End Response With Custom Response

--- SchoolProject.Api/Controllers/StudentsController.cs
+++ SchoolProject.Api/Controllers/StudentsController.cs
@@ -1,6 +1,7 @@
 ﻿using MediatR;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using SchoolProject.Api.Base;
 using SchoolProject.Core.Features.Students.Commands.Models;
 using SchoolProject.Core.Features.Students.Queries.Models;
 using SchoolProject.Data.AppMetaData;
@@ -9,14 +10,9 @@ namespace SchoolProject.Api.Controllers
 {
     //[Route("api/[controller]")]
     [ApiController]
-    public class StudentsController : ControllerBase
+    public class StudentsController : AppControllersBase
     {
-        private readonly IMediator _mediator;
-
-        public StudentsController(IMediator mediator)
-        {
-            this._mediator = mediator;
-        }
+        
 
         [HttpGet(PathRoute.StudentsRoute.List)]
         public async Task<IActionResult> GetStudentList()
@@ -30,14 +26,14 @@ namespace SchoolProject.Api.Controllers
         {
             var responsestudent = await _mediator.Send(new StudentByIdQuery { id = id });
             if (responsestudent == null) NotFound(responsestudent);
-            return Ok(responsestudent);
+            return NewResult(responsestudent);
         }
 
         [HttpPost(PathRoute.StudentsRoute.Create)]
         public async Task<IActionResult> CreateStudent([FromBody]StudentCommandRequest command)
         {
             var responseaddStudent =await  _mediator.Send(command);
-            return Ok(responseaddStudent);
+            return NewResult(responseaddStudent);
 
         }
     }
